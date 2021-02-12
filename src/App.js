3e097e49fc315e403ee3460be06f388eaa513d5f
Ilan Manuel Naibryf <inaibryf@uchicago.edu>
2021-02-12 12:27:41 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from "./main"
+function App() {
+  return (
+    <div id="div10">
+    <Main/>
+    </div>
+  );
+}
+
+export default App;
